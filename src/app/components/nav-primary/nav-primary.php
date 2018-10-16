d877da6830cc0f41919ab1d25e0245e2852ae5bd
arniebradfo <arniebradfo@gmail.com>
2018-10-16 11:34:27 +0300

    nav css start

--- src/app/components/nav-primary/nav-primary.php
+++ src/app/components/nav-primary/nav-primary.php
@@ -1,20 +1,27 @@
 <button class="nav__button button--icon">
-	<?php echo trns_icon_component(['name'=>'Menu']) ?>
+	<?php echo trns_icon_component(['name'=>'Menu', 'class'=>'nav__open-icon']) ?>
+	<?php // echo trns_icon_component(['name'=>'X', 'class'=>'nav__close-icon']) ?>
 </button>
 
-<nav class="nav">
-
-	<h2 class="nav_title">
-		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+<header class="nav">
+		
+	<div class="nav__header">
+		<a class="nav_title" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
 			<?php bloginfo( 'name' ); ?>
 		</a>
-	</h2>
-
+	</div>
+	
 	<?php wp_nav_menu( array(
-		'theme_location' => 'primary',
-		'menu_class'     => 'nav__menu'
+		'theme_location'  => 'primary',
+		'container_class' => 'nav__menu',
+		'container'       => 'nav'
+		
 	)); ?>
 
-	<?php get_search_form(); ?>
+	<div class="nav__footer">
+		<?php get_search_form(); ?>
+	</div>	
 
-</nav>
+</header>
+
+<div class="nav__overlay"></div>
