5ada1c350d5ee1587f8112d0fa2dc523fb3734aa
arniebradfo <arniebradfo@gmail.com>
2019-01-22 11:49:06 -0700

    master is now the compiled release branch

---
+++ icon.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * icon component
+ *
+ * @link ???
+ *
+ * @package WordPress
+ * @subpackage Transitions
+ * @since 1.0
+ * @version 1.0
+ */
+
+
+function trns_icon_component( $atts, $content=null, $tag='' ) {
+
+	$output = '<!-- icon.php -->';
+	$output.= '<svg ';
+	$atts['class'] = isset($atts['class']) ? "icon {$atts['class']}" : 'icon';
+	foreach($atts as $att => $val)
+		if ($att == 'name') continue;
+		$output .= " $att=\"$val\""; // echo all attributes from the shorcode
+	$output .= '>';
+	$output .= "<use xlink:href=\"#icon_{$atts['name']}\"></use>"; 
+	$output .= '</svg>';
+	$output .= '<!--/ icon.php -->';
+	return $output;
+}
+// add_shortcode( 'tnst-icon', 'trns_icon_component' );
+
+?>
